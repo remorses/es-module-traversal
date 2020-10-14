86841e4a67b5499010ea385c761cd72f0f70a514
remorses <beats.by.morse@gmail.com>
2020-10-14 16:53:49 +0200

    tests/simple:

--- tests/simple.ts
+++ tests/simple.ts
@@ -1,13 +1,11 @@
+import { init } from 'es-module-lexer'
 import path from 'path'
 import { walkEsModulesSync } from '../src'
-import { from } from 'ix/iterable'
-import { init, parse } from 'es-module-lexer'
-import assert from 'assert'
 
 it('works', async () => {
     await init
     const currentFile = path.resolve(__dirname, __filename)
     walkEsModulesSync({
-        entryPoint: path.resolve(__dirname, __filename),
+        entryPoint: currentFile,
     }).forEach((x) => console.log(x.importPath))
 })
