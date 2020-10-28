7cbfcd7448bd197fdc664b607313f99ea4186a1b
Tommaso De Rossi <beats.by.morse@gmail.com>
2020-10-28 12:11:43 +0100

    refactor

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,6 @@ import os from 'os'
 export const PACKAGE_NAME = 'es-module-traversal'
 
 export const MAX_IO_OPS: number = os.cpus().length * 4
+
+
+export const JS_EXTENSIONS = new Set(['.js', '.jsx', '.ts', '.tsx', '.mjs', '.cjs'])
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './traverse'
+export { isRelative, defaultReadFile, defaultResolver } from './support'
 export * from './server-functions'
