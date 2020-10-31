ab7ef58f11aab892ef0041242f8e1b493bdccb13
Tommaso De Rossi <beats.by.morse@gmail.com>
2020-10-31 15:58:28 +0100

    add second arg to readfile

--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,8 @@ export type TraverseArgs = {
     stopTraversing?: (importPath: string, context: string) => boolean
     resolver?: (cwd: string, id: string) => string
     // | ((cwd: string, id: string) => Promise<string>)
-    readFile?: ((path: string, importer?: string) => string) | ((path: string) => Promise<string>)
+    readFile?:
+        | ((path: string, importer?: string) => string)
+        | ((path: string, context?: string) => Promise<string>)
     onFile?: ((path: string) => any) | ((path: string) => Promise<any>)
 }
