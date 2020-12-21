39076ce14674e4df85b87914e098f9373faa497e
remorses <beats.by.morse@gmail.com>
2020-12-21 16:34:18 +0100

    fix esbuild tests

--- src/esbuild/traverseEsbuild.ts
+++ src/esbuild/traverseEsbuild.ts
@@ -90,7 +90,7 @@ export async function traverseWithEsbuild({
                                     }
                                 }
                             },
-                            onUnresolved: () => {
+                            onNonResolved: () => {
                                 return {
                                     external: true,
                                 }
